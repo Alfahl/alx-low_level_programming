dc36ce87673aaa4cbee0d036ffa2b0f549b117c7
Alfahl <hoobgame081@gmail.com>
2023-08-10 04:42:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-entry point in the program
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	/*your code here*/
+	return (0);
+}
+
