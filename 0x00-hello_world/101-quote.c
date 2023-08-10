b9aa659c615889100c0d3acf1f5fd50cccce1204
Alfahl <hoobgame081@gmail.com>
2023-08-10 14:53:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main-entry of the program
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+	return (0);
