d0c30793deb98d5dd822e8df7cc4e254905f047d
Alfahl <hoobgame081@gmail.com>
2023-08-13 23:39:36 -0700

    2-print_alphabet.c'

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-entry of the program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ana;
+
+	for (ana = 'a'; ana <= 'z'; ana++)
+	{
+		putcahr(ana);
+	}
+	{
+		puchar('\n');
+	}
+	return (0);
+}
