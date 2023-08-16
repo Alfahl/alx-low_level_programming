218ecde278347c54b5fa34deb96020018f2f170f
Alfahl <hoobgame081@gmail.com>
2023-08-16 05:32:14 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign-print + if the 
+ *
+ * @n: takes the you
+ *
+ * Return: always 0
+ */
+int print_sign(n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
