223d197dbe74abcfff497e07da62dfc9bd5d4191
Alfahl <hoobgame081@gmail.com>
2023-08-16 04:38:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main-entry of the program
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+
