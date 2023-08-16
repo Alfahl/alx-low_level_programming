255043efcb8679b3cd786ce6197640dd202586b2
Alfahl <hoobgame081@gmail.com>
2023-08-16 04:52:03 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-entry of the program
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; lin <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	}
+}
+
