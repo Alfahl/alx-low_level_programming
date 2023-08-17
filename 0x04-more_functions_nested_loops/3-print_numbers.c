b1e5b101d4a8c91a865f31f2fdf0a19fbc63dedc
Alfahl <hoobgame081@gmail.com>
2023-08-17 04:39:47 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	chae c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
